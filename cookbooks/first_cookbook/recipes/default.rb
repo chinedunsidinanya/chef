4724ef18b2da289fd5bc70842277d0a70e453e17
chinedunsidinanya <34515761+chinedunsidinanya@users.noreply.github.com>
2021-07-24 21:17:22 -0500

    adding apache and first_cookbooks

---
+++ cookbooks/first_cookbook/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: first_cookbook
+# Recipe:: default
+#
+# Copyright (c) 2021 The Authors, All Rights Reserved.
+
+include_recipe "first_cookbook::first_recipe"
+
+
+
---
+++ cookbooks/first_cookbook/recipes/first_recipe.rb
@@ -0,0 +1,31 @@
+file '/hello.txt' do
+     content 'Hello I am editing this file WOrld!'
+end
+template '/var/www/html/index.html' do
+    action  :create
+    source  'index.html.erb'
+end
+
+template '/etc/motd' do
+    action :create
+    source 'first_template.erb'
+end
+
+package 'httpd' do
+    action :install
+end
+
+package 'git' do
+    action :install
+end
+
+package 'tree' do
+
+    action :install
+end
+
+service 'httpd' do
+
+    action [:enable, :start]
+
+end
