adeb77955efca100961f07ad8ab8f6b8df1fb16c
Chinedu Nsidinanya <chineducarson@icloud.com>
2021-07-25 00:13:03 -0500

    modified my first_cookboof file

--- cookbooks/first_cookbook/recipes/first_recipe.rb
+++ cookbooks/first_cookbook/recipes/first_recipe.rb
@@ -1,11 +1,3 @@
-file '/hello.txt' do
-     content 'Hello I am editing this file WOrld!'
-end
-template '/var/www/html/index.html' do
-    action  :create
-    source  'index.html.erb'
-end
-
 template '/etc/motd' do
     action :create
     source 'first_template.erb'
@@ -29,3 +21,14 @@ service 'httpd' do
     action [:enable, :start]
 
 end
+
+file '/hello.txt' do
+     content 'Hello I am editing this file WOrld!'
+end
+
+template '/var/www/html/index.html' do
+    action  :create
+    source  'index.html.erb'
+end
+
+
